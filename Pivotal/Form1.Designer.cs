225e53b32e6dad066151233c01ce3a5802fba867
HubertWlo <h.wloszek@gmail.com>
2024-04-23 17:54:53 +0200

    Dodaj pliki projektów.

---
+++ Pivotal/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Pivotal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rozpocznij obliczenia";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(351, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Wartości zapamiętane w trakcie ostatniego cyklu";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 80);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Zatrzymaj obliczenia";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 421);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Pivotal/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using OfficeOpenXml;
+using PrimeFinder;
+
+namespace Pivotal
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private List<Prime> primeResults;
+        private string xmlFilePath = "PrimeNumbers.xml";
+        private int counter = 1;
+        public bool stopAutoSave = false;
+        private Prime prime;
+        public Form1()
+        {
+            primeResults = new List<Prime>();
+            InitializeComponent();
+            InitializeTimer();
+            InitializePrime();
+        }
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Tick += Timer_Tick;
+        }
+        private void InitializePrime()
+        {
+            prime = new Prime();
+            prime.PrimeCalculated += Prime_PrimeCalculated;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            if (!stopAutoSave)
+            {
+                int currentPrime;
+                if (int.TryParse(label1.Text, out currentPrime))
+                {
+                    prime.Calculate(currentPrime);
+                }
+            }
+            if (stopAutoSave)
+            {
+                timer.Stop();
+                SaveXmlFile();
+            }
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            timer.Interval = 60000; //!-----------
+            stopAutoSave = false;
+            timer.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stopAutoSave = true;
+            prime.StopCalculating();
+        }
+        private void SaveXmlFile()
+        {
+            List<Prime> existingResults = new List<Prime>();
+            if (File.Exists(xmlFilePath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Prime>));
+                using (FileStream fs = new FileStream(xmlFilePath, FileMode.Open))
+                {
+                    existingResults = (List<Prime>)serializer.Deserialize(fs);
+                }
+            }
+            existingResults.AddRange(primeResults);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Prime>));
+            using (FileStream fs = new FileStream(xmlFilePath, FileMode.Create))
+            {
+                xmlSerializer.Serialize(fs, existingResults);
+            }
+            MessageBox.Show("Plik XML zapisany pomyślnie.", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            primeResults.Clear();
+        }
+        private void Prime_PrimeCalculated(int primeNumber, string elapsedTimeCycle, int cycle, string elapsedTime)
+        {
+            if (primeNumber == -1)
+            {
+                MessageBox.Show("Nie udało się znaleźć liczby.", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                stopAutoSave = true;
+                prime.StopCalculating();
+            }
+            label1.Text = primeNumber.ToString();
+            label2.Text = "Wartości zapamiętane w trakcie ostatniego cyklu:";
+            primeResults.Add(new Prime { PrimeNumber = primeNumber, ElapsedTimeCycle = elapsedTimeCycle + "m", Cycle = cycle , ElapsedTime = elapsedTime + "ms"});
+        }
+    }
+}
+namespace PrimeFinder
+{
+    public class Prime
+    {
+        public int PrimeNumber;
+        public string ElapsedTimeCycle;
+        public string ElapsedTime;
+        public int Cycle = 0;
+        private bool stopCalculating = false;
+        public event Action<int, string, int, string> PrimeCalculated;
+        public void StopCalculating()
+        {
+            stopCalculating = true;
+        }
+        public void Calculate(int prime)
+        {
+            stopCalculating = false;
+            Stopwatch stopwatch = new Stopwatch();
+            Stopwatch stopwatchCycle = new Stopwatch();
+            stopwatchCycle.Start();
+            int nextPrime = FindPrime(prime, stopwatchCycle, stopwatch);
+            stopwatchCycle.Stop();
+            PrimeCalculated?.Invoke(nextPrime, stopwatchCycle.Elapsed.TotalMinutes.ToString("0"), Cycle, stopwatch.Elapsed.TotalMilliseconds.ToString("0.00000"));
+        }
+        public int FindPrime(int last, Stopwatch stopwatchCycle, Stopwatch stopwatch)
+        {
+            bool done = false;
+            stopwatchCycle.Start();
+            int nextNumber = last + 1;
+            int number = nextNumber;
+            while (stopwatchCycle.Elapsed.TotalMinutes < 2 && !stopCalculating) //!!!!
+            {
+                stopwatch.Start();
+                if (IsPrime(nextNumber))
+                {
+                    number = nextNumber;
+                    done = true;
+                    stopwatch.Reset();
+
+                }
+                nextNumber++;
+            }
+            this.Cycle++;
+            stopwatchCycle.Stop();
+            if (done)
+            {
+                stopwatch.Stop();
+                return number;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public bool IsPrime(int n)
+        {
+            if (n <= 1) return false;
+            if (n <= 3) return true;
+            if (n % 2 == 0 || n % 3 == 0) return false;
+            int i = 5;
+            while (i * i <= n)
+            {
+                if (n % i == 0 || n % (i + 2) == 0) return false;
+                i += 6;
+            }
+            return true;
+        }
+    }
+}
